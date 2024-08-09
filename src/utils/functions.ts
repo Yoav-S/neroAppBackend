a9f0f3de86ea00238ac5f44438e97641c030ce3b
Yoav-S <99165953+Yoav-S@users.noreply.github.com>
2024-08-09 11:53:25 +0300

    backend main commit

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,41 +1,47 @@
 import { Filter } from 'mongodb';
 import { Filters } from './interfaces';
 
-
-
 export async function createFilterQuery(filters: Filters, categoriesCollection: any): Promise<Filter<any>> {
     const filterQuery: Filter<any> = {};
-  
-    if (filters.location && (filters.location.city || filters.location.district)) {
-      filterQuery['location.city'] = filters.location.city ? new RegExp(filters.location.city, 'i') : { $exists: true };
-      if (filters.location.district) {
-        filterQuery['location.district'] = new RegExp(filters.location.district, 'i');
-      }
+
+    // Location filtering
+    if (filters.location) {
+        if (filters.location.city) {
+            filterQuery['location.city'] = new RegExp(filters.location.city, 'i');
+        }
+        if (filters.location.district) {
+            filterQuery['location.district'] = new RegExp(filters.location.district, 'i');
+        }
     }
-  
-    // Distance filter (placeholder)
-    if (filters.distance && (filters.distance.min !== undefined || filters.distance.max !== undefined)) {
-      // Implement distance filtering logic here
+
+    // Distance filter (to be implemented based on your distance logic)
+    if (filters.distance) {
+        if (filters.distance.min !== undefined || filters.distance.max !== undefined) {
+            // Implement distance filtering logic here based on your data structure
+        }
     }
-  
+
+    // Post type filtering
     if (filters.type && filters.type.length > 0) {
-      filterQuery.postType = { $in: filters.type };
+        filterQuery.postType = { $in: filters.type };
     }
-  
+
+    // Category filtering
     if (filters.category && filters.category.length > 0) {
-      const categoryIds = await categoriesCollection.find({ name: { $in: filters.category } }).toArray();
-      filterQuery.category = { $in: categoryIds.map((cat: any) => cat._id) };
+        const categoryIds = await categoriesCollection.find({ name: { $in: filters.category } }).toArray();
+        filterQuery.category = { $in: categoryIds.map((cat: any) => cat._id) };
     }
-  
-    if (filters.date && filters.date.length > 0) {
-      // Implement date filtering logic based on your requirements
-      // This is a placeholder
-      filterQuery.createdAt = { $gte: new Date(filters.date[0]), $lte: new Date(filters.date[1]) };
+
+    // Date filtering (placeholder logic)
+    if (filters.date && filters.date.length === 2) {
+        const [startDate, endDate] = filters.date;
+        filterQuery.createdAt = { $gte: new Date(startDate), $lte: new Date(endDate) };
     }
-  
+
+    // Size filtering
     if (filters.size && filters.size.length > 0) {
-      filterQuery.size = { $in: filters.size };
+        filterQuery.size = { $in: filters.size };
     }
-  
+
     return filterQuery;
-  }
+}
